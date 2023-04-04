ee169a9509d50b2062792aa9a93d5c65f93b0f6d
Sergio Sanchez <ssanchezfilho@gmail.com>
2023-04-04 13:31:11 -0300

    added react-card-flip library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 
+import ReactCardFlip from 'react-card-flip';
+
 import MaskedInputCardNumber from './components/inputCardNumber';
 import MaskedInputValidThru from './components/inputValidThru';
-
 
 import { ReactComponent as Visa } from './assets/visa.svg';
 import { ReactComponent as ContactlessPayment } from './assets/ContactlessPayment.svg';
@@ -43,6 +44,18 @@ function App() {
     const capitalizedInputName = inputName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
     setName(capitalizedInputName);
   }
+
+  const [ isFlipped, setIsFlipped ] = useState(false);
+  
+  const handleFlip = (event) => {
+    if (isFlipped == false) {
+      setIsFlipped(true)
+    } else {
+      setIsFlipped(false)
+    }
+  }
+
+  const [ cvv, setCVV ] = useState('');
 
 
 
@@ -90,49 +103,70 @@ function App() {
 
             <div className='sm:order-last sm:py-4 sm:flex sm:flex-col items-center justify-around sm:gap-8'>
               <div className='relative'>
-                <section className='w-[280px] h-[168px] sm:mb-8 bg-black mx-auto sm:mx-0 rounded-2xl border-[#374151] border-[1px] pt-4 pb-6 px-6' id='card'>
-                  <div className='flex items-center justify-between mb-10'>
-                    <Visa />
-                    <ContactlessPayment />
-                  </div>
 
-                    {
-                      cardNumber == ''
-                      ?
-                      <div className='flex justify-between text-white mb-6 font-semibold tracking-[.25em]'>
-                        <div>4716</div>
-                        <div>8039</div>
-                        <div>02&bull;&bull;</div>
-                        <div>&bull;&bull;&bull;&bull;</div>
-                      </div>
-                      :
-                      <div className='flex justify-between text-white mb-6 font-semibold tracking-[.25em]'>
-                        <div>{cardNumberArray1}</div>
-                        <div>{cardNumberArray2}</div>
-                        <div>{cardNumberArray3}</div>
-                        <div>{cardNumberArray4}</div>
-                      </div>
-                    }
 
-                  <div className='text-[#F9FAFB] text-sm flex justify-between'>
-                    {name == '' ? <p>Seu nome aqui</p> : <p>{name}</p>}
-                    {
-                      validThru == ''
-                      ?
-                      <div className='flex justify-between text-white mb-6 tracking-[.25em]'>
-                        <span>&bull;&bull;</span>
-                        <span>/</span>
-                        <span>&bull;&bull;</span>
+                <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+                  {/* FRONT OF THE CARD */}
+                  <section className='w-[280px] h-[168px] sm:mb-8 bg-[url("/src/assets/bg-card.png")] mx-auto sm:mx-0 rounded-2xl border-[#374151] border-[1px] pt-4 pb-6 px-6' id='card'>
+                    <div className='flex items-center justify-between mb-10'>
+                      <Visa />
+                      <ContactlessPayment />
+                    </div>
+
+                      {
+                        cardNumber == ''
+                        ?
+                        <div className='flex justify-between text-white mb-6 font-semibold tracking-[.25em]'>
+                          <div>4716</div>
+                          <div>8039</div>
+                          <div>02&bull;&bull;</div>
+                          <div>&bull;&bull;&bull;&bull;</div>
+                        </div>
+                        :
+                        <div className='flex justify-between text-white mb-6 font-semibold tracking-[.25em]'>
+                          <div>{cardNumberArray1}</div>
+                          <div>{cardNumberArray2}</div>
+                          <div>{cardNumberArray3}</div>
+                          <div>{cardNumberArray4}</div>
+                        </div>
+                      }
+
+                    <div className='text-[#F9FAFB] text-sm flex justify-between'>
+                      {name == '' ? <p>Seu nome aqui</p> : <p>{name}</p>}
+                      {
+                        validThru == ''
+                        ?
+                        <div className='flex justify-between text-white mb-6 tracking-[.25em]'>
+                          <span>&bull;&bull;</span>
+                          <span>/</span>
+                          <span>&bull;&bull;</span>
+                        </div>
+                        :
+                        <div className='flex justify-between text-white mb-6 tracking-[.25em]'>
+                          <span>{month}</span>
+                          <span>/</span>
+                          <span>{year}</span>
+                        </div>
+                      }
+                    </div>
+                  </section>
+
+                  {/* BACK OF THE CARD */}
+                  <section className='w-[280px] h-[168px] sm:mb-8 bg-[url("/src/assets/bg-card.png")] mx-auto sm:mx-0 rounded-2xl border-[#374151] border-[1px] pt-4 pb-6 ' id='card'>
+                      <div className='bg-[#111827] w-full h-8 flex'></div>
+                      <div className='flex px-6 gap-2 mt-[46px] items-center'>
+                        {
+                          cvv == ''
+                          ?
+                          <div className='bg-[#D1D5DB] w-[200px] flex justify-end px-4 tracking-[.25em] items-center rounded'>&bull;&bull;&bull;</div>
+                          :
+                          <div className='bg-[#D1D5DB] w-[200px] flex justify-end px-4 tracking-[.25em] rounded'>{cvv}</div>
+                        }
+                        <p className='text-white text-sm'>CVV</p>
                       </div>
-                      :
-                      <div className='flex justify-between text-white mb-6 tracking-[.25em]'>
-                        <span>{month}</span>
-                        <span>/</span>
-                        <span>{year}</span>
-                      </div>
-                    }
-                  </div>
-                </section>
+                  </section>
+                </ReactCardFlip>
+
 
               </div>
 
@@ -155,7 +189,7 @@ function App() {
                 
                 <div className='flex flex-col'>
                   <label for='name' className='text-[#E5E7EB] font-semibold text-sm mb-1 flex items-center'>CVV &nbsp;<Question /></label>
-                  <input name='name' id='name' type="text" maxLength='3' minLength='3' placeholder='***' className='border-[#374151] w-[129px] border-[1px] rounded p-4 bg-[#101827] text-white box-border focus:outline-none focus:ring-2 focus:ring-[#9747FF] hover:ring-2 hover:ring-[#374151] invalid:border-[#FB7185]'/>
+                  <input onFocus={handleFlip} onBlur={handleFlip} onChange={(event) => setCVV(event.target.value)} name='name' id='name' type="text" maxLength='3' minLength='3' placeholder='***' className='border-[#374151] w-[129px] border-[1px] rounded p-4 bg-[#101827] text-white box-border focus:outline-none focus:ring-2 focus:ring-[#9747FF] hover:ring-2 hover:ring-[#374151] invalid:border-[#FB7185]'/>
                 </div>
               </div>
 
